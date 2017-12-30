017a4059dd19170f201276733566f6b2884ebcc2
David Yonwook Chung <yon_gospel@hotmail.com>
2017-12-29 19:58:39 -0800

    installed angularjs dependency. working display.

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 angular.module('app', [])
+  .component('todoList', {
+      template:`
+        <div>
+            <h1>Todo List</h1>
+            <input>
+            <button>add</button>
+            <ul>
+                <li></li>
+            </ul>
+            <hr/>
+            <pre></pre>
+        </div>>
+      `
+  })
